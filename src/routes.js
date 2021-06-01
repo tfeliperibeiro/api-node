ce77e0d612705935101ccd9fd5946a7347506719
Thiago Felipe <56236974+tfeliperibeiro@users.noreply.github.com>
2021-06-01 11:35:12 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ import HandleDB from './controllers/HandleDB';
 const routes = new Router();
 
 routes.get('/buscar', HandleDB.getDados);
-routes.put('/atualizar/id', HandleDB.updateDados);
+routes.put('/atualizar/:id', HandleDB.updateDados);
 routes.post('/criar', HandleDB.createDados);
-routes.delete('/deletar/id', HandleDB.deleteDados);
+routes.delete('/deletar/:id', HandleDB.deleteDados);
 
 export default routes;
